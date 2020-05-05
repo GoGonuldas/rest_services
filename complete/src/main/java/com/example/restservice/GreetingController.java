cc2f0ffcaa627b91375b868af392ca98223182c0
grkngnlds <grkngnlds@gmail.com>
2020-05-05 12:59:38 +0300

    files

---
+++ complete/src/main/java/com/example/restservice/GreetingController.java
@@ -0,0 +1,33 @@
+package com.example.restservice;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class GreetingController {
+
+	private static final String template = "Hello, %s!";
+	private final AtomicLong counter = new AtomicLong();
+
+	@GetMapping("/greeting")
+	public Greeting greeting(@RequestParam(value = "name", defaultValue = "GET REQUEST") String name) {
+		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+	}
+
+	@PostMapping("/greeting")
+	public Greeting creategreeting(@RequestParam(value = "name", defaultValue = "POST REQUEST") String name) {
+		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+	}
+
+	@PutMapping("/greeting")
+	public Greeting updategreeting(@RequestParam(value = "name", defaultValue = "PUT REQUEST") String name) {
+		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+	}
+
+	@DeleteMapping("/greeting")
+	public Greeting deletegreeting(@RequestParam(value = "name", defaultValue = "DELETE REQUEST") String name) {
+		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+	}
+
+}
